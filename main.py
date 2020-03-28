0f4213586e215d02234ffd39cba291964d19c437
jackjieS <563255057@qq.com>
2020-03-28 21:05:34 +1100

    1

--- main.py
+++ main.py
@@ -1,32 +1,23 @@
 # -*- coding: utf-8 -*-
 from contextlib import closing
 import requests
-from flask import Flask, request, Response,abort
-from urllib.parse import urlparse
+from flask import Flask, request, Response
+
 app = Flask(__name__)
 
 
-@app.before_request
-def proxy():
-    #if not filter(request):
-    #    return abort(403)
-    headers = {h[0]: h[1] for h in request.headers}
-    url=request.url.replace(request.host+'/', '', 1)
-    print(url)
-    # 一些自己的逻辑...
+@app.route("/path/<path:path>")
+def before_request(path):
 
-    return requests.request(request.method,url, data=request.json, headers=headers).content
-
-
-def filter(request):
-    if( 'bilibili.com/bangumi/play/' in request.url):
-        return True
-    else:
-        return False
+    data = request.data or request.form or None
+    headers = dict()
+    for name, value in request.headers:
+        if name == 'Host':
+            continue
+        headers[name] = value
+    r = requests.get(url=path, headers=headers, data=data)
+    return r.content
 
 
-app.run(
-    host = '0.0.0.0',
-    port = 7777,  
-    debug = True 
-    )
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=7777)
