bb35dde36bb82415d48b48dd87260a51980e7792
jackjieS <563255057@qq.com>
2020-03-28 21:14:37 +1100

    1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import Flask, request, Response
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources=r'/*')
 
 @app.route("/path/<path:path>")
 def before_request(path):
